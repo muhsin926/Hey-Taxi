1cd27f32e43460f79b1d0f1ab8f9683977b76bd7
muhsin926 <muhsinmp926@gmail.com>
2023-02-22 14:08:40 +0530

    add edit option in vehicle

--- driver/src/components/Vehicle/Vehicle.jsx
+++ driver/src/components/Vehicle/Vehicle.jsx
@@ -1,30 +1,43 @@
-import axios from 'axios'
-import React, { useCallback, useEffect, useState } from 'react'
-import Button from '../Button/Button'
-import url from "../../api/Api"
-import Modal from './Modal'
-import { useDispatch, useSelector } from 'react-redux'
-import { setShowModal } from '../../redux/slices/ModalSlice'
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import url from "../../api/Api";
+import Modal from "./Modal";
+import { useDispatch, useSelector } from "react-redux";
+import { setShowModal } from "../../redux/slices/ModalSlice";
 
 const Vehicle = () => {
-  const [vehicles, setVehicles] = useState([])
-  const { showModal } = useSelector((state) => state.modal)
-  const dispatch = useDispatch()
+  const [vehicles, setVehicles] = useState([]);
+  const [editV, setEditV] = useState({})
+  const { showModal } = useSelector((state) => state.modal);
+  const dispatch = useDispatch();
 
   const getVehicles = useCallback(async () => {
-    const { data } = await axios.get(`${url}/api/driver/get-vehicles`)
-    setVehicles(data.vehicles)
-  }, [])
-
-  useEffect(() => {
+    const { data } = await axios.get(`${url}/api/driver/get-vehicles`);
+    setVehicles(data.vehicles);
+  }, [vehicles]);
+  
+  const handleEdit = (id) => {
+    let editTo = vehicles.filter((vehi) => vehi._id == id)
+    editTo = editTo[0]
+    setEditV(editTo)
+    dispatch(setShowModal())
+  }
+   useEffect(() => {
     getVehicles()
-  }, [])
+   },[])
   return (
     <div>
-      <div className='flex justify-end'>
-        <button className='py-1 px-3 bg-black text-white rounded' onClick={() => dispatch(setShowModal())}>Add Vehicle</button>
+      <div className="flex justify-end">
+        <button
+          className="py-1 px-3 bg-black text-white rounded"
+          onClick={() => dispatch(setShowModal())}
+        >
+          Add Vehicle
+        </button>
       </div>
-      {showModal && <Modal />}
+      {showModal && <Modal edit={ editV} />}
       <section>
         <div className="relative overflow-x-auto">
           <table className="w-full text-sm text-left">
@@ -56,10 +69,16 @@ const Vehicle = () => {
                   <td className="px-6 py-4">{vehicle.model}</td>
                   <td className="px-6 py-4">{vehicle.reg_no}</td>
                   <td className="px-6 py-4">
-                    <img className='w-12' src={vehicle.insurence} alt="" />
+                    <img className="w-12" src={vehicle.insurence} alt="" />
                   </td>
                   <td className="px-6 py-4">
-                    <img className='w-12' src={vehicle.RC} alt="" />
+                    <img className="w-12" src={vehicle.RC} alt="" />
+                  </td>
+                  <td className="px-6 py-4">
+                    <FontAwesomeIcon onClick={() => handleEdit(vehicle._id)} icon={faPenToSquare} />
+                  </td>
+                  <td className="px-6 py-4">
+                    <FontAwesomeIcon className="text-red-500" icon={faTrash} />
                   </td>
                 </tr>
               ))}
@@ -68,7 +87,7 @@ const Vehicle = () => {
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Vehicle
+export default Vehicle;
